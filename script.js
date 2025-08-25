fefc508a2243fbdf789fad4f9ae696fa004824df
Bekam Berhanu <Bebabekam@gmail.com>
2025-08-25 19:05:31 +0300

    uI and logic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+ //for smooth view ui slide 
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
   console. log(entry)
@@ -10,3 +12,45 @@ const observer = new IntersectionObserver((entries) => {
   }) ;
   const hiddenElements = document.querySelectorAll('.hidden');
   hiddenElements.forEach((el) => observer.observe(el));
+
+  //geting HTml element  access to script 
+
+    const fromCurrency = document.getElementById("from");  
+  const toCurrency = document.getElementById("to");     
+  const amount = document.getElementById("amount");       
+  const convertBtn = document.getElementById("convert");  
+  const result = document.getElementById("result");       
+
+  async function loadCurrencies() {
+    let res = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
+    let data = await res.json();
+
+    let currencies = Object.keys(data.rates);
+
+    currencies.forEach(currency => {
+      fromCurrency.innerHTML += `<option value="${currency}">${currency}</option>`;
+      toCurrency.innerHTML += `<option value="${currency}">${currency}</option>`;
+    });
+      console.log(data);
+  }
+
+  loadCurrencies();
+
+
+  //convert function 
+
+    async function convert() {
+    let res = await fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency.value}`);
+    let data = await res.json();
+
+    let rate = data.rates[toCurrency.value];
+    let inputAmount = amount.value;
+
+    let converted = (inputAmount * rate).toFixed(2);
+
+    result.innerText = `${inputAmount} ${fromCurrency.value} = ${converted} ${toCurrency.value}`;
+  }
+
+  convertBtn.addEventListener("click", convert);
+
+//the inp
